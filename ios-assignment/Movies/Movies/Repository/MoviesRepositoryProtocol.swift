e191f0613c6a2910533c4eb1084efb3bdf98a07d
Sytac Dev <31990977+sytac-bot@users.noreply.github.com>
2022-02-25 18:31:59 +0800

    Initial devcase commit

---
+++ ios-assignment/Movies/Movies/Repository/MoviesRepositoryProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  MoviesRepositoryProtocol.swift
+//  Movies
+//
+//  Created by xdmgzdev on 14/04/2021.
+//
+
+import Foundation
+
+protocol MoviesRepositoryProtocol {
+  func getPopular()
+  func getTopRated()
+}
